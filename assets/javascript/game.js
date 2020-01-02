ca72919f612900e1e267881e837c2540e0a67fb5
stylianos kouroupas <steliosk9@gmail.com>
2020-01-02 13:44:43 -0600

    Create the fighter variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    var yodaAttack = 8;
+    var yodaCounter = 11;
+    var yodaHP = 65;
+    
+    var lukeAttack = 5;
+    var lukeCounter = 10;
+    var lukeHP = 65;
+    
+    var vaderAttack = 7;
+    var vaderCounter = 12;
+    var vaderHP = 60;
+    
+    var kyloAttack = 6;
+    var kyloCounter = 9;
+    var kyloHP = 65;
+    
+    var numOfWarriors = 4
+    
+    var masterYoda = false;
+    var lukeSkywalker = false;
+    var darthVader = false;
+    var kyloRen = false;
+    
+    var player = false;
+    var playerAttack = 0;
+    var playerHP = 0;
+    
+    var enemy = false;
+    var enemyCounter = 0;
+    var enemyHP = 0
+
+
+
+
+
+})
